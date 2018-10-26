596a65390429b0f765084a19b0ba319ce4d8dbce
Watt Erikson <turtle@turtle-coin.com>
2018-10-26 16:13:39 -0700

    Rebrand dashboard page

--- dashboard/js/dashBoard.js
+++ dashboard/js/dashBoard.js
@@ -25,54 +25,56 @@ function getParameterByName(name, url) {
 }
 var code = getParameterByName('code');
 
-$(document).ready(function() {
-if(getCookie("token") == "" || getCookie("token") == null ) {
-  if(code) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://api.twitchturtle.com/code", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        code: code
-    }));
-    xhr.onload = function() {
-      profile = JSON.parse(this.responseText);
-      if (this.status == 400) {
-          swal("Error!", profile.error, "error").then(function() {
-            window.location = "https://www.streamlabs.com/api/v1.0/authorize?client_id=e4lKhBGqlUblZ8JhIdW1jCvRqrQ6k4OjRSUcazTE&redirect_uri=https://twitchturtle.com/dashboard/&response_type=code&scope=donations.create";
-          });
+function tokensOrBust() {
+  if(getCookie("token") == "" || getCookie("token") == null ) {
+    if(code) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://api.twitchturtle.com/code", true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(JSON.stringify({
+          code: code
+      }));
+      xhr.onload = function() {
+        profile = JSON.parse(this.responseText);
+        if (this.status == 400) {
+          window.location = "https://www.streamlabs.com/api/v1.0/authorize?client_id=e4lKhBGqlUblZ8JhIdW1jCvRqrQ6k4OjRSUcazTE&redirect_uri=https://twitchturtle.com/dashboard/&response_type=code&scope=donations.create";
+        }
+        document.cookie = "token="+profile.token;
+        window.history.pushState({}, document.title, "/dashboard" + "");
+        submit();
+        // document.getElementById("address").innerHTML = profile.address;
       }
-      document.cookie = "token="+profile.token;
-
-      submit();
-      // document.getElementById("address").innerHTML = profile.address;
-    }
+    } else {
+      window.location.replace("https://www.streamlabs.com/api/v1.0/authorize?client_id=e4lKhBGqlUblZ8JhIdW1jCvRqrQ6k4OjRSUcazTE&redirect_uri=https://twitchturtle.com/dashboard/&response_type=code&scope=donations.create")
+    };
   } else {
-    window.location.replace("https://www.streamlabs.com/api/v1.0/authorize?client_id=e4lKhBGqlUblZ8JhIdW1jCvRqrQ6k4OjRSUcazTE&redirect_uri=https://twitchturtle.com/dashboard/&response_type=code&scope=donations.create")
-  };
-} else {
-  setInterval(submit(), 10000);
+    setInterval(submit(), 10000);
+  }
 }
-});
-
 
 var profile;
 function submit() {
-  $.each(transactionsJSON(), function(index, value){
-    if (value.transactions !== undefined && value.transactions.length != 0) {
-      $.each(value.transactions, function(index2, value2){
-        $('#rows').append(
-          '<tr>' +
-            '<td sorttable_customkey="' + value2.timestamp + '">'+moment.unix((value2.timestamp)).fromNow()+'</td>' +
-            '<td>'+convertExtraToName(value2.extra)+'</a></td>' +
-            '<td>'+convertExtraToMessage(value2.extra)+'</td>' +
-            '<td>'+(value2.amount/100).toFixed(2)+'</td>' +
-          '</tr>'
-        );
-      });
-    }
+  $("#rows tr").detach();
+  $.getJSON('https://api.coinmarketcap.com/v2/ticker/2958/?convert=USD', function(data) {
+    $.each(transactionsJSON(), function(index, value){
+      if (value.transactions !== undefined && value.transactions.length != 0) {
+        $.each(value.transactions, function(index2, value2){
+          console.log(value.transactions)
+          $('#rows').append(
+            '<tr>' +
+              '<td sorttable_customkey="' + value2.timestamp + '">'+moment.unix((value2.timestamp)).fromNow()+'</td>' +
+              '<td>'+convertExtraToName(value2.extra)+'</a></td>' +
+              '<td>'+convertExtraToMessage(value2.extra)+'</td>' +
+              '<td>'+(value2.amount/100).toFixed(2)+'</td>' +
+              '<td>'+(data.data.quotes.USD.price * (value2.amount/100)).toFixed(6)+'</td>' +
+            '</tr>'
+          );
+        });
+      };
+    });
+    sorttable.innerSortFunction.apply(document.getElementById('time'), [])
   });
-  sorttable.innerSortFunction.apply(document.getElementById('time'), [])
-  sorttable.innerSortFunction.apply(document.getElementById('time'), [])
+
 }
 
 function hex2a(hexx) {
@@ -131,9 +133,9 @@ function transactionsJSON() {
         resp = xmlHttp.responseText;
     }
     if(xmlHttp.status == 401) {
-      swal("Error!", 'Incorrect Username/Password', "error").then(() => {
-        modal.style.display='block'
-      });
+      console.log('Invalid tokens, clearing cookies and trying again');
+      document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      tokensOrBust();
     }
     json = JSON.parse(resp);
     document.getElementById("name").innerHTML = json.name;
@@ -147,3 +149,73 @@ function transactionsJSON() {
 
     return json.transactions;
     }
+
+function withdraw(address) {
+
+  address = address.replace(/\s/g,'');
+
+  var resp ;
+  var xmlHttp ;
+  resp  = '' ;
+  xmlHttp = new XMLHttpRequest();
+  var token = getCookie("token");
+  if (token == "" || token == null) {
+      return
+  }
+
+  console.log(address);
+  if(!address) {
+    swal("Error!", "You must enter an address to withdraw to!", "error");
+    return
+  }
+
+  if(!/^TRTL[0-9A-Z]{95}$/gmi.test(address)) {
+    console.log('Invalid address!');
+    swal("Invalid address!", "", "error",);
+    return
+  }
+  if(address === document.getElementById('address').innerHTML) {
+    console.log('Invalid address!');
+    swal("Withdraw address cannot be your TwitchTurtle address", "", "error");
+    return
+  }
+
+  swal({
+    title: 'Are you sure the information is correct?',
+    text: "Withdraw to " + address + "\n PLEASE NOTE: THIS IS IRREVERSABLE!\n YOU CANNOT SEND DIRECTLY TO AN EXCHANGE!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      if(xmlHttp != null)
+      {
+          xmlHttp.open( "GET", "https://api.twitchturtle.com/withdraw/" + address.replace(/\s/g,''), false );
+          xmlHttp.setRequestHeader('Content-Type', 'application/json');
+          xmlHttp.setRequestHeader("Authorization", "Basic " + btoa(token + ":" + 'nonce'));
+          xmlHttp.send( null );
+          resp = xmlHttp.responseText;
+      }
+      if(xmlHttp.status == 401) {
+        console.log('Invalid tokens, clearing cookies and trying again');
+        document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        tokensOrBust();
+      }
+      if(xmlHttp.status == 400) {
+        console.log('Error');
+        swal(
+          'Error!',
+          JSON.parse(resp).error,
+          'error'
+        )
+      }
+      json = JSON.parse(resp);
+      return json;
+    } else {
+      swal("Withdraw Cancelled!", "", "error",);
+    }
+  });
+}
+
+$(document).ready(function() {tokensOrBust()});
